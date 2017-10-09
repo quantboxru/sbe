96815b90d89474c573d456f4a8b92dd8328b1834
Osman <mrlvyonok@gmail.com>
2017-10-09 16:57:55 +0300

    Basic header implementation.

---
+++ solution/car.h
@@ -0,0 +1,59 @@
+#include <string.h>
+#include <stdint.h>
+
+typedef struct
+{
+  uint16_t blockLength;
+  uint16_t templateId;
+  uint16_t schemaId;
+  uint16_t version;
+} messageHeader;
+
+typedef int64_t Int64;
+
+typedef struct
+{
+  int64_t mantissa;
+  int8_t exponent;
+} decimal;
+
+typedef uint16_t ModelYear;
+typedef char VehicleCode[6];
+typedef int32 someNumbers[5];
+
+typedef struct
+{
+  uint16_t capacity;
+  uint8_t numCylinders;
+  const uint16_t maxRpm;
+} Engine;
+
+typedef enum
+{
+  F = 0,
+  T = 1
+} BooleanType;
+
+typedef enum
+{
+  A = 'A',
+  B = 'B',
+  C = 'C'
+} Model;
+
+// Types section ended.
+
+typedef enum {
+  sbe_msg_type_RequiredPrimitives = 10,
+  sbe_msg_type_OptionalPrimitives = 2,
+  sbe_msg_type_Car = 3
+} sbe_msg_type_t;
+
+typedef struct sbe_RequiredPrimitives_s sbe_RequiredPrimitives_t;
+typedef struct sbe_OptionalPrimitives_s sbe_OptionalPrimitives_t;
+typedef struct sbe_Car_s sbe_Car_t;
+
+void* sbe_create(sbe_msg_type_t);
+void* sbe_decode(char* buf, size_t len);
+
+// а дальше - геттеры и сеттеры.
